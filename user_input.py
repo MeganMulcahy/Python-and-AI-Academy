a5d9b929d5e520fa294425452dc2c2c4f10c90df
Megan Mulcahy <70993260+MeganMulcahy@users.noreply.github.com>
2026-06-08 12:52:40 -0500

    python user inputs and string castings

---
+++ user_input.py
@@ -0,0 +1,17 @@
+# The following program demonstrate how to cast integers and strings
+number = input("Enter an integer: ")
+
+# String to Integer
+new_int = int(number)
+print(new_int + 50)
+
+# Integer to String
+new_string = (str(50) + " is a number in string format")
+print(number + new_string)
+# The following program takes a user input.
+
+# Get User Input
+name = input("Enter the character's name: ")
+print("Name: " + name)
+age = input("Enter the character's age: ")
+print("Age: " + age + " years old")
