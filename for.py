54b8908afb414fb92913c854208fb1b6ad3909ce
Megan Mulcahy <70993260+MeganMulcahy@users.noreply.github.com>
2026-07-05 21:47:28 -0500

    for loops and data

--- for.py
+++ for.py
@@ -91,3 +91,56 @@ last_item = range(1, 11)[0]
 print(last_item)  # Outputs: 1
 last_item = range(1, 11)[-1]
 print(last_item)  # Outputs: 10
+
+for number in range(6):
+    #print("#" * number)
+    #print(2*number-1) # as number = 1, 2*1-1 = 1, 2*2-1 = 3, etc.
+    print(" " * (6 - number) + "#" * (2 * number - 1))
+
+for number in range(6):
+    print(f"{number} x {number} = {number * number}")
+
+list = ['Python', 'Numpy','Pandas','Django', 'Flask']
+for item in list:
+    print(item)
+
+for num in range(1,10,2):
+    print(num)  # This will raise an error because 'int' object is not iterable
+
+for num in range(9,0,-2):
+    print(num) 
+
+sum = 0
+sumeven = 0
+sumodd = 0
+for num in range(0,101):
+    sum += num
+    if num % 2 == 0:
+        sumeven += num
+    else:
+        sumodd += num
+print("The sum is:", sum)
+print("The sum of even numbers is:", sumeven)
+print("The sum of odd numbers is:", sumodd)
+
+from datafiles.countries_data import countries
+
+unique_languages = set() #bc sets dont keep duplicates!
+for country in countries:
+    unique_languages.update(country['languages']) #update() adds elements to a set, but only if they are not already present
+print("# of Unique languages:", len(unique_languages))
+
+print("10 Most Popular Languages:")
+language_counts = {}
+for country in countries:
+    for language in country['languages']:
+        language_counts[language] = language_counts.get(language, 0) + 1 # This avoids key errors by safely setting a language's count to 0 the first time it is found before adding 1
+sorted_languages = sorted(language_counts.items(), key=lambda item: item[1], reverse=True) # tells Python to sort the items based on their counts (the numbers) instead of their names (the text),
+print("Most spoken languages:")
+for language, count in sorted_languages[:10]:
+    print(f"{language}: {count} countries")
+    
+print("10 Most Populated Countries:")
+sorted_pop = sorted(countries, key = lambda country: country['population'], reverse=True)
+for country in sorted_pop[:10]:
+    print(f"{country['name']}: {country['population']}")
